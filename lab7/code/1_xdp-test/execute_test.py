d0a7b373e3432a1a2dfc2161429079ee15b0cf6c
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2022-12-21 10:09:38 +0100

    Testing Script

--- lab7/code/1_xdp-test/execute_test.py
+++ lab7/code/1_xdp-test/execute_test.py
@@ -1,25 +1,31 @@
-#PASS (key 0)
-#dummy ARP request for ip address 0.0.0.0
-p=Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(op=1)
-#dummy ipv6 packet
-p=Ether(dst="00:0c:29:76:28:bf")/IPv6()
-#TX (key 2)
-#exact match
-#dummy IPv4 packet to 8.8.8.8
-p=Ether(dst="00:0c:29:76:28:bf")/IP(src="7.7.7.7",dst="8.8.8.8")
-#LPM match
-#dummy IPv4 packet from 10.0.0.1
-p=Ether(dst="00:0c:29:76:28:bf")/IP(src="10.0.0.1",dst="1.1.1.1")
-#dummy IPv4 packet from 192.168.0.1
-p=Ether(dst="00:0c:29:76:28:bf")/IP(src="192.168.0.1",dst="1.1.1.1")
-#DROP (key 1)
-p=Ether(dst="00:0c:29:76:28:bf")/IP(src="1.1.1.1",dst="2.2.2.2")
-#try and send a ping. arp is passed (response received)
-#ping is dropped (no response is received)
-#ping 172.16.115.2
-#to send packet through proper iface
-#my VM is attached to a host only net. On the host iface=bridge100
+import scapy
+import time
+import os
+
+# PASS (key 0)
+# dummy ARP request for ip address 0.0.0.0
+p = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.ARP(op=1)
+# dummy ipv6 packet
+p = scapy.Ether(dst="00:0c:29:76:28:bf")/scapy.IPv6()
+# TX (key 2)
+# exact match
+# dummy IPv4 packet to 8.8.8.8
+p = scapy.Ether(dst="00:0c:29:76:28:bf")/scapy.IP(src="7.7.7.7", dst="8.8.8.8")
+# LPM match
+# dummy IPv4 packet from 10.0.0.1
+p = scapy.Ether(dst="00:0c:29:76:28:bf") / \
+    scapy.IP(src="10.0.0.1", dst="1.1.1.1")
+# dummy IPv4 packet from 192.168.0.1
+p = scapy.Ether(dst="00:0c:29:76:28:bf") / \
+    scapy.IP(src="192.168.0.1", dst="1.1.1.1")
+# DROP (key 1)
+p = scapy.Ether(dst="00:0c:29:76:28:bf")/scapy.IP(src="1.1.1.1", dst="2.2.2.2")
+# try and send a ping. arp is passed (response received)
+# ping is dropped (no response is received)
+# ping 172.16.115.2
+# to send packet through proper iface
+# my VM is attached to a host only net. On the host iface=bridge100
 #sendp(p, iface="bridge100")
 
-sleep(3)
-system("bpftool map dump name stats_db")
+time.sleep(3)
+os.system("bpftool map dump name stats_db")
